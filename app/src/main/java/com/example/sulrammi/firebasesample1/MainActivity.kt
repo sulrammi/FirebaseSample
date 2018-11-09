23e8d337e3d1232e4604325d7eb6cd1d1d42eccb
Junsuk Oh <a811219@gmail.com>
2018-11-09 13:22:33 +0900

    Firebase RealtimeDatabase 연습

--- app/src/main/java/com/example/sulrammi/firebasesample1/MainActivity.kt
+++ app/src/main/java/com/example/sulrammi/firebasesample1/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.sulrammi.firebasesample1
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val database = FirebaseDatabase.getInstance()
+    private val myRef = database.getReference("message")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            myRef.setValue(editText.text.toString())
+        }
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue(String::class.java)
+                textView.text = value
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+            }
+        })
     }
 }
